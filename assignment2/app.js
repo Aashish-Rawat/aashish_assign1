7d24cab546117469c9e8c81eefc3955f677c242a
shshrwt <aashish.rawat07@gmail.com>
2016-09-18 13:23:24 +0530

    some fixes

--- assignment2/app.js
+++ assignment2/app.js
@@ -6,12 +6,14 @@
   .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
 
+
+  //controller for buy item
   ToBuyShoppingCtrl.$inject = ['ShoppingListCheckOffService'];
   function ToBuyShoppingCtrl(ShoppingListCheckOffService) {
-    var showList = this;
-    showList.items = ShoppingListCheckOffService.getItems();
-    showList.removeItem = function (itemIndex) {
-    var CurrentObj =  showList.items[itemIndex];
+    var buyList = this;
+    buyList.itemsBuy = ShoppingListCheckOffService.getItems();
+    buyList.removeItem = function (itemIndex) {
+    var CurrentObj =  buyList.itemsBuy[itemIndex];
     ShoppingListCheckOffService.addItem(CurrentObj.name, CurrentObj.quantity);
     ShoppingListCheckOffService.removeItem(itemIndex);
   };
@@ -19,10 +21,11 @@
   }
 
 
+  //controller for bought item
   AlreadyBoughtShoppingCtrl.$inject = ['ShoppingListCheckOffService'];
   function AlreadyBoughtShoppingCtrl(ShoppingListCheckOffService) {
-    var showList12 = this;
-    showList12.items = ShoppingListCheckOffService.getItemsBuy();
+    var boughtList = this;
+    boughtList.itemsBought = ShoppingListCheckOffService.getItemsBuy();
   }
 
 
@@ -30,34 +33,37 @@
   function ShoppingListCheckOffService() {
     var service = this;
 
-    // List of shopping items
-    var items = [{name: "itemName1",quantity: 12},
-                {name: "itemName2",quantity: 13},
-                {name: "itemName3",quantity: 14},
-                {name: "itemName4",quantity: 15},
-                {name: "itemName5",quantity: 16},
-                {name: "itemName6",quantity: 17},
-                 ];
+    // List of Buy items
+    var itemsBuy = [{name: "Kit Kat",quantity: 11},
+                {name: "Cadbury",quantity: 13},
+                {name: "Toblerone",quantity: 17},
+                {name: "Patchi",quantity: 7},
+                {name: "Ferrero Rocher",quantity: 1}];
 
-    var itemsBuy = [];
+    var itemsBought = []; //bought list init
+
+    //add item in bought list
     service.addItem = function (itemName, quantity) {
       var item = {
         name: itemName,
         quantity: quantity
       };
-      itemsBuy.push(item);
+      itemsBought.push(item);
     };
 
+    //remove item from buy list afer bought
     service.removeItem = function (itemIdex) {
-      items.splice(itemIdex, 1);
+      itemsBuy.splice(itemIdex, 1);
     };
 
+    //get bought item list
     service.getItemsBuy = function () {
-      return itemsBuy;
+      return itemsBought;
     };
 
+    //git buy items list
     service.getItems = function () {
-      return items;
+      return itemsBuy;
     };
   }
 /***************************** service method end *****************************/
