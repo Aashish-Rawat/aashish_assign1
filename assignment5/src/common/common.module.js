24c510284b76d7413b9800ee526594324243b82d
Aashish Rawat <aashish.rawat07@gmail.com>
2018-03-26 15:18:29 +0530

    api cahnges

--- assignment5/src/common/common.module.js
+++ assignment5/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://mysterious-taiga-72259.herokuapp.com/menu_items/')
+.constant('ApiPath', 'https://mysterious-taiga-72259.herokuapp.com/')
 .config(config);
 
 config.$inject = ['$httpProvider'];
